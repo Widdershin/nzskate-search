d155c6e30e8b336f7f20700403110ebb83b8b3ed
Nick Johnstone <ncwjohnstone@gmail.com>
2014-05-18 17:02:59 +1200

    Started working on the UB plugin

--- skate_search/store_plugins.py
+++ skate_search/store_plugins.py
@@ -1,3 +1,5 @@
+from pyquery import PyQuery
+
 
 class UltimateBoards(object):
 
@@ -5,3 +7,14 @@ class UltimateBoards(object):
 
     search_url = "http://www.ultimateboards.co.nz/search/products/{query}"\
         .format
+
+    def search_shop(self, query):
+        query_url = self.search_url(query=query)
+        query = PyQuery(url=query_url)
+
+        results = self.parse_result_html(query(".galleryImageListItem"))
+
+        return results
+
+    def parse_result_html(self, html):
+        raise NotImplemented
