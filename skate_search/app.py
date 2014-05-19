0e365d088b3a0eaa2f92198729807b034614f0d8
Nick Johnstone <ncwjohnstone@gmail.com>
2014-05-19 23:27:58 +1200

    Added fuzzy sorting for results

--- skate_search/app.py
+++ skate_search/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from store_plugins import (UltimateBoards, HyperRide,
                            BasementSkate, TerrabangSkate)
 import json
+from fuzzywuzzy import fuzz
 
 app = Flask(__name__)
 
@@ -15,9 +16,20 @@ def homepage():
 
 @app.route('/search')
 def search():
+    query = request.args["query"]
+
     results = []
     for plugin in plugins:
-        results.extend(plugin.search_shop(request.args["query"]))
+        results.extend(plugin.search_shop(query))
+
+    results = sorted(
+        results,
+        key=lambda x: fuzz.partial_ratio(query, x.name),
+        reverse=True
+    )
+
+    for listing in results:
+        print("{}: {}".format(listing.name.strip(), fuzz.partial_ratio(query, listing.name)))
 
     return json.dumps([listing.to_dict() for listing in results])
 
